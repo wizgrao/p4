00e86cfe4d8b68fd41b78d5513ceba92b5a44ab1
Ubuntu <gaurav@gaurav-test2.kxqzmqydd0gezcntqxoldbs1zb.bx.internal.cloudapp.net>
2020-03-31 01:17:56 +0000

    change model more

--- part2/train.py
+++ part2/train.py
@@ -25,24 +25,26 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.n_class = N_CLASS
         self.layers = nn.Sequential(
-            #########################################
-            ###        TODO: Add more layers      ###
-            #########################################
             nn.Conv2d(3, 64, 5, padding=2),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(64),
             nn.Conv2d(64, 128, 5, padding=2),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(128, 128, 5, padding=2),
+            nn.BatchNorm2d(128),
+            nn.Conv2d(128, 256, 5, padding=2),
             nn.ReLU(),
-            nn.Conv2d(128, 128, 5, padding=2),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 512, 5, padding=2),
             nn.ReLU(),
             nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.Conv2d(128, 128, 5, padding=2),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512, 512, 5, padding=2),
             nn.ReLU(),
             nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.Conv2d(128, self.n_class, 1, padding=0),
+            nn.BatchNorm2d(512),
+            nn.Conv2d(512, self.n_class, 3, padding=1),
             nn.ReLU(inplace=True)
         )
 
